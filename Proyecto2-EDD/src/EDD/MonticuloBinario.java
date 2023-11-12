cb11440918831931f0a0260f9bcec0d7407aedef
AngeloooG <129348581+AngeloooG@users.noreply.github.com>
2023-11-11 22:04:20 -0400

    Update MonticuloBinario.java

--- Proyecto2-EDD/src/EDD/MonticuloBinario.java
+++ Proyecto2-EDD/src/EDD/MonticuloBinario.java
@@ -9,5 +9,91 @@ package EDD;
  * @author joses
  */
 public class MonticuloBinario {
-    
+
+    private int[] monticulo;
+    private int capacidad;
+    private int tamaño;
+
+    public MonticuloBinario (int capacidad) {
+        this.capacidad = capacidad;
+        this.tamaño = 0;
+        this.monticulo = new int[capacidad];
+    }
+
+    public int obtenerPadre(int i) {
+        return (i - 1) / 2;
+    }
+
+    public int obtenerHijoIzquierdo(int i) {
+        return 2 * i + 1;
+    }
+
+    public int obtenerHijoDerecho(int i) {
+        return 2 * i + 2;
+    }
+
+    public void intercambiar(int i, int j) {
+        int temp = monticulo[i];
+        monticulo[i] = monticulo[j];
+        monticulo[j] = temp;
+    }
+
+    public void insertar(int elemento) {
+        if (tamaño >= capacidad) {
+            System.out.println("El montículo está lleno, no se puede insertar.");
+            return;
+        }
+
+        monticulo[tamaño] = elemento;
+        int indice = tamaño;
+        tamaño++;
+
+        while (indice > 0 && monticulo[indice] < monticulo[obtenerPadre(indice)]) {
+            intercambiar(indice, obtenerPadre(indice));
+            indice = obtenerPadre(indice);
+        }
+    }
+
+    public int eliminarMinimo() {
+        if (tamaño <= 0) {
+            throw new IllegalStateException("El montículo está vacío, no se puede eliminar el mínimo.");
+        }
+
+        int minimo = monticulo[0];
+        monticulo[0] = monticulo[tamaño - 1];
+        tamaño--;
+        Orden(0);
+
+        return minimo;
+    }
+
+    public void Orden(int indice) {
+        int indiceMinimo = indice;
+        int izquierdo = obtenerHijoIzquierdo(indice);
+        int derecho = obtenerHijoDerecho(indice);
+
+        if (izquierdo < tamaño && monticulo[izquierdo] < monticulo[indiceMinimo]) {
+            indiceMinimo = izquierdo;
+        }
+
+        if (derecho < tamaño && monticulo[derecho] < monticulo[indiceMinimo]) {
+            indiceMinimo = derecho;
+        }
+
+        if (indice != indiceMinimo) {
+            intercambiar(indice, indiceMinimo);
+            Orden(indiceMinimo);
+        }
+    }
+
+    public boolean estaVacio() {
+        return tamaño == 0;
+    }
+
+    public void imprimirMonticulo() {
+        for (int i = 0; i < tamaño; i++) {
+            System.out.print(monticulo[i] + " ");
+        }
+        System.out.println();
+    }
 }
