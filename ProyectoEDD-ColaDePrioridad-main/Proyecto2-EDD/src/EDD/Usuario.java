2be05d648626fa5c13e86a1cb1dd49919136db61
fefi05 <129181491+fefi05@users.noreply.github.com>
2023-11-16 22:17:53 -0400

    Add files via upload

--- ProyectoEDD-ColaDePrioridad-main/Proyecto2-EDD/src/EDD/Usuario.java
+++ ProyectoEDD-ColaDePrioridad-main/Proyecto2-EDD/src/EDD/Usuario.java
@@ -24,4 +24,53 @@ public class Usuario {
       
     }
    
+    public boolean esVacio() {
+        return this.pPrim == null;
+}
+    public Documento primero() {
+        return pPrim;
+    }
+    
+    public void proximo(Documento pValor) {
+        if (pValor != null && pValor.getpSig() != null) {
+            pValor = pValor.getpSig();
+        }
+    }
+    
+    public void insertar(String nombre, int tamaño, String tipo) {
+        Documento nuevoDocumento = new Documento(nombre, tamaño, tipo);
+        if (this.esVacio()) {
+            this.pPrim = nuevoDocumento;
+        } else {
+            Documento pAux = this.pPrim;
+            while (pAux.getpSig() != null) {
+                pAux = pAux.getpSig();
+            }
+            pAux.setpSig(nuevoDocumento);
+        }
+        this.iN++;
+    }
+    
+    public void eliminar(Documento elemento) {
+        if (pPrim == null || elemento == null) {
+            return;
+        }
+
+        if (pPrim.gettInfo().equals(elemento)) {
+            pPrim = pPrim.getpSig();
+            iN--;
+            return;
+        }
+
+        Documento actual = pPrim;
+        while (actual.getpSig() != null && !actual.getpSig().gettInfo().equals(elemento)) {
+            actual = actual.getpSig();
+        }
+
+        if (actual.getpSig() != null) {
+            actual.setpSig(actual.getpSig().getpSig());
+            iN--;
+        }
+    }
+    
 }
